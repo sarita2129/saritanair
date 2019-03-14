57d12548f381a250f9c7b108732d968ad1ba1b44
sarita2129 <nairsarita2129@gmail.com>
2019-03-15 00:30:00 +1100

    my first commit for portfolio

---
+++ js/index.js
@@ -0,0 +1,81 @@
+var $cont = document.querySelector('.cont');
+var $elsArr = [].slice.call(document.querySelectorAll('.el'));
+var $closeBtnsArr = [].slice.call(document.querySelectorAll('.el__close-btn'));
+var $imgtictacdiv = document.querySelector('.imgtictactoe');
+// alert('1');
+setTimeout(function() {
+  $cont.classList.remove('s--inactive');
+}, 200);
+$elsArr.forEach(function($el) {
+  $el.classList.add('el-height');
+
+});
+$elsArr.forEach(function($el) {
+  $el.addEventListener('click', function() {
+    if (this.classList.contains('s--active')) return;
+    $cont.classList.add('s--el-active');
+    this.classList.add('s--active');
+    this.classList.remove('el-height');
+    this.classList.add('el-height-click');
+    // alert(this.height());
+    // alert($window.height());
+  });
+});
+
+$closeBtnsArr.forEach(function($btn) {
+  $btn.addEventListener('click', function(e) {
+    e.stopPropagation();
+    $cont.classList.remove('s--el-active');
+    document.querySelector('.el.s--active').classList.remove('s--active');
+    $elsArr.forEach(function($el) {
+      $el.classList.remove('el-height-click');
+      $el.classList.add('el-height');
+
+    });
+
+  });
+});
+$(document).ready(function(){
+//
+//   $('.divproject').hover(function(){
+//
+//     // alert(this);
+//     this.querySelector('.content').classList.add('content-hover');
+//     this.querySelector('.content').classList.remove('content');
+//     this.classList.add('imgtictactoe-hover');
+//     this.classList.remove('imgtictactoe');
+//   });
+
+$(".imgtictactoe").hover(function(){
+    // $('.content').show();
+    // alert(this.querySelector('.content'));
+    $(this).find('.content').first().show();
+},function(){
+    // $('.content').hide();
+    $(this).find('.content').first().hide();
+});
+$(".imggostyle").hover(function(){
+    // $('.content').show();
+    // alert(this.querySelector('.content'));
+    $(this).find('.content').first().show();
+},function(){
+    // $('.content').hide();
+    $(this).find('.content').first().hide();
+});
+$(".imgreelsplex").hover(function(){
+    // $('.content').show();
+    // alert(this.querySelector('.content'));
+    $(this).find('.content').first().show();
+},function(){
+    // $('.content').hide();
+    $(this).find('.content').first().hide();
+});
+$(".imgfoodie").hover(function(){
+    // $('.content').show();
+    // alert(this.querySelector('.content'));
+    $(this).find('.content').first().show();
+},function(){
+    // $('.content').hide();
+    $(this).find('.content').first().hide();
+});
+})
